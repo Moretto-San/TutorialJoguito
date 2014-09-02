989303238270d43aa895f0e7676f4c9f243c7037
Moretto-San <rafael.morett@gmail.com>
2014-09-02 15:19:37 -0300

    .

---
+++ KiloboltGame/src/kiloboltgame/framework/Animation.java
@@ -0,0 +1,17 @@
+package kiloboltgame.framework;
+
+import java.util.ArrayList;
+
+public class Animation {
+
+	private ArrayList frames;
+	private int currentFrame;
+	private long animTime; // long takes up more memory than int but can hold
+							// more accurate numbers.
+	private long totalDuration;
+
+	public Animation() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
